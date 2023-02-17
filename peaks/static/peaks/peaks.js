cbfa064abaa17b114e5d7fcc21a106e506a9b414
Florian Neukirchen <mail@riannek.de>
2023-02-17 17:45:40 +0100

    show markers for new waypoints

--- peaks/static/peaks/peaks.js
+++ peaks/static/peaks/peaks.js
@@ -1,3 +1,5 @@
+var map; // global
+
 $(document).ready(function(){
     // Select order
     $("#id_order").change(function(){
@@ -20,7 +22,7 @@ $(document).ready(function(){
             zoom = 8
         } 
 
-        var map = L.map('map').setView([lat, lon], zoom);
+        map = L.map('map').setView([lat, lon], zoom);
 
         var osmlayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -73,7 +75,7 @@ $(document).ready(function(){
 
         console.log(`tour ${tour}`)
         // Tour: show waypoint markers
-        if (mode === "tour"){
+        if (mode === "tour" || mode === "edittour"){
             $.get(`/waypoints/${tour}`, function(data, status){
                 
                 var gjsonlayer = L.geoJSON(data, {
@@ -82,9 +84,24 @@ $(document).ready(function(){
             });
         }
 
-        // Tour edit: listen to map click events
+        // Tour edit 
         if (mode === "edittour"){
+            // listen to map click events
             map.on('click', addWaypoint);
+
+            // init nr and lat, lon in the table
+            $(".wp").each(function(index) {
+                // Get data
+                const lat = parseFloat($(this).find("input[name*='lat']").val());
+                const lon = parseFloat($(this).find("input[name*='lon']").val());
+
+                // Write data if lat, lon are defined
+                if (!isNaN(lat)){
+                    $(this).find(".wpnumber").text(`${index + 1}`);
+                    $(this).find('.latlon').html('<small>' + lat.toFixed(4) + ', ' + lon.toFixed(4) + '</small>');
+                }
+
+            });
         }
 
     }
@@ -248,42 +265,45 @@ function onEachFeature2(feature, layer) {
     
 }
 
+L.NumberedDivIcon = L.Icon.extend({
+    options: {
+    iconUrl: '/static/peaks/wpmarker.png',
+    number: '',
+    shadowUrl: null,
+    iconSize: new L.Point(25, 41),
+    iconAnchor: new L.Point(13, 41),
+    popupAnchor: new L.Point(0, -33),
+    /*
+    iconAnchor: (Point)
+    popupAnchor: (Point)
+    */
+    className: 'leaflet-div-icon'
+    },
+    
+    createIcon: function () {
+    var div = document.createElement('div');
+    var img = this._createImg(this.options['iconUrl']);
+    var numdiv = document.createElement('div');
+    numdiv.setAttribute ( "class", "number" );
+    numdiv.innerHTML = this.options['number'] || '';
+    div.appendChild ( img );
+    div.appendChild ( numdiv );
+    this._setIconStyles(div, 'icon');
+    return div;
+    },
+    
+    //you could change this to add a shadow like in the normal marker if you really wanted
+    createShadow: function () {
+    return null;
+    }
+    });
+
+
 function onEachWaypoint(feature, layer) {
 
     // http://blog.charliecroom.com/index.php/web/numbered-markers-in-leaflet
 
-    L.NumberedDivIcon = L.Icon.extend({
-        options: {
-        iconUrl: '/static/peaks/wpmarker.png',
-        number: '',
-        shadowUrl: null,
-        iconSize: new L.Point(25, 41),
-        iconAnchor: new L.Point(13, 41),
-        popupAnchor: new L.Point(0, -33),
-        /*
-        iconAnchor: (Point)
-        popupAnchor: (Point)
-        */
-        className: 'leaflet-div-icon'
-        },
-        
-        createIcon: function () {
-        var div = document.createElement('div');
-        var img = this._createImg(this.options['iconUrl']);
-        var numdiv = document.createElement('div');
-        numdiv.setAttribute ( "class", "number" );
-        numdiv.innerHTML = this.options['number'] || '';
-        div.appendChild ( img );
-        div.appendChild ( numdiv );
-        this._setIconStyles(div, 'icon');
-        return div;
-        },
-        
-        //you could change this to add a shadow like in the normal marker if you really wanted
-        createShadow: function () {
-        return null;
-        }
-        });
+
 
     layer.bindTooltip(feature.properties.name);
     var icon = new L.NumberedDivIcon({number: `${feature.properties.number}`})
@@ -308,21 +328,24 @@ function addWaypoint(e) {
     // Update index numbers 
     var total = $('#id_form-TOTAL_FORMS').val();
     oldElement.find("input[name*='number']").val(total);
+    oldElement.find('.wpnumber').text(`${total}`);
     newElement.find(':input').each(function() {
         var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
         var id = 'id_' + name;
         $(this).attr({'name': name, 'id': id}).val('');
     });
     
-    // Update management form and wp number
+    // Show marker
+    new L.Marker(e.latlng, {
+        icon: new L.NumberedDivIcon({number: `${total}`})
+        }).addTo(map);;
+
+    
+    // Update management form 
     total++;
     $('#id_form-TOTAL_FORMS').val(total);
-    newElement.find('.wpnumber').text(`${total}`);
+    
 
     // Insert new form
     oldElement.after(newElement);
-
-
-
-    
 }
